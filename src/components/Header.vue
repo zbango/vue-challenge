3d74cb40d6d7561c8e8c5c8e799ea72ba656f3c3
Steven Tabango <steven.tabango@gmail.com>
2023-05-29 19:40:01 -0500

    feat: Create Header component

---
+++ src/components/Header.vue
@@ -0,0 +1,59 @@
+<template>
+  <header class="header">
+    <div class="left-content">
+      <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="48" height="48" />
+      <div class="header-text">
+        tipti-vue-challenge
+      </div>
+    </div>
+
+    <div class="right-content">
+    <router-link to="/" class="link">Inicio</router-link>
+    <router-link to="/about" class="link">Ayuda</router-link>
+  </div>
+  </header>
+</template>
+
+<style scoped>
+.header {
+  background-color: #003B95;
+  padding: 15px;
+  width: 100%;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.left-content {
+  display: flex;
+  align-items: center;
+}
+
+.right-content {
+  display: flex;
+  align-items: center;
+}
+
+.logo {
+  margin-right: 10px;
+}
+
+.header-text {
+  color: #fff;
+  font-size: large;
+}
+
+.link {
+  color: #fff;
+  text-decoration: none;
+  margin-left: 12px;
+  font-size: large;
+}
+
+@media (max-width: 600px) {
+  .header-text {
+    display: none;
+  }
+}
+</style>
