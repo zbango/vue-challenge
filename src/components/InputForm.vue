0640c645d8003c750c66a96dd7c55a7475b69129
Steven Tabango <steven.tabango@gmail.com>
2023-05-31 00:45:25 -0500

    chore: update styles in InputForm component

--- src/components/InputForm.vue
+++ src/components/InputForm.vue
@@ -4,7 +4,7 @@
       <div class="card-body">
         <div class="form-group">
           <label for="customer-type">Tipo de cliente:</label>
-          <div class="radio-group">
+          <div class="radio-group" id="customer-type">
             <input type="radio" id="regular" value="regular" v-model="hotelStore.customerType" />
             <label for="regular">Regular</label>
             <input type="radio" id="rewards" value="rewards" v-model="hotelStore.customerType" />
@@ -13,21 +13,50 @@
         </div>
 
         <div class="form-group">
-          <label for="dates">Fechas:</label>
-          <div v-for="(date, index) in hotelStore.dates" :key="index" class="date-input">
+          <label class="tooltip" for="dates" id="date-section">
+            Fechas:
+            <i class="fas fa-question-circle"></i>
+            <span class="tooltip-text">Ingrese las fechas en las que desea reservar</span>
+          </label>
+          <h4 v-if="!!hotelStore.error" class="error-message">
+            <i class="fas fa-times"></i>
+            {{ hotelStore.error }}
+          </h4>
+          <div v-for="(date, index) in hotelStore.dates" :key="index" class="date">
             <input
               type="date"
               :value="date"
               @input="hotelStore.updateDate($event.target.value, index)"
               placeholder="Ingrese una fecha"
+              class="input-date"
             />
-            <button @click="hotelStore.removeDate(index)" class="remove-date-btn">Eliminar</button>
+            <button
+              v-if="hotelStore.dates.length > 1 && index !== 0"
+              @click="hotelStore.removeDate(index)"
+              class="remove-date-btn"
+              aria-label="Eliminar fecha"
+            >
+              <i class="fas fa-trash-alt" alt="Icono de Eliminar"></i>
+            </button>
           </div>
-          <button @click="hotelStore.addDate" class="add-date-btn">Agregar Fecha</button>
+          <button
+            @click="hotelStore.addDate"
+            class="add-date-btn"
+            aria-label="Agregar fecha"
+            id="add-btn"
+          >
+            Agregar fecha
+          </button>
         </div>
-
         <div class="form-group">
-          <button @click="hotelStore.calculateCheapestHotel()">Buscar opción más económica</button>
+          <button
+            id="search-btn"
+            @click="hotelStore.calculateCheapestHotel()"
+            class="calculate-btn"
+            aria-label="Buscar"
+          >
+            Buscar
+          </button>
         </div>
       </div>
     </div>
@@ -42,10 +71,11 @@ const hotelStore = useHotelStore()
 
 <style scoped>
 .card {
-  border: 1px solid #ccc;
+  border: 1px solid #ddd;
   border-radius: 5px;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
   width: 300px;
+  padding: 20px;
 }
 
 .card-container {
@@ -54,12 +84,8 @@ const hotelStore = useHotelStore()
   margin-top: 30px;
 }
 
-.card-body {
-  padding: 20px;
-}
-
 .form-group {
-  margin-bottom: 10px;
+  margin-bottom: 15px;
 }
 
 .radio-group {
@@ -67,21 +93,118 @@ const hotelStore = useHotelStore()
   gap: 10px;
 }
 
-button {
-  padding: 5px 10px;
+label {
+  font-weight: bold;
 }
 
-.date-input {
+input[type='radio'] {
+  margin-right: 5px;
+}
+
+button {
+  padding: 8px 15px;
+  border: none;
+  color: #fff;
+  border-radius: 3px;
+  cursor: pointer;
+  transition: background-color 0.3s;
+  height: 36px;
+}
+
+button:hover {
+  background-color: #0056b3;
+}
+
+.date {
   display: flex;
   align-items: center;
-  margin-bottom: 10px;
+  margin-bottom: 15px;
+}
+
+.input-date {
+  width: 100%;
+  border: none;
+  border-bottom: 1px solid #ccc;
+  padding: 5px 0;
+  font-size: 16px;
+  outline: none;
 }
 
 .remove-date-btn {
   margin-left: 10px;
+  padding: 4px 10px;
+  background-color: #dc3545;
+}
+
+.remove-date-btn:hover {
+  background-color: #c82333;
 }
 
 .add-date-btn {
   margin-top: 10px;
+  padding: 4px 10px;
+  background-color: #28a745;
+  width: 100%;
+}
+
+.add-date-btn:hover {
+  background-color: #218838;
+}
+
+.calculate-btn {
+  padding: 8px 15px;
+  background-color: #003b95;
+  color: #fff;
+  border-radius: 3px;
+  cursor: pointer;
+  transition: background-color 0.3s;
+  width: 100%;
+}
+
+.calculate-btn:hover {
+  background-color: #0f4492;
+}
+
+.tooltip {
+  display: flex;
+  align-items: center;
+  position: relative;
+  cursor: help;
+}
+
+.tooltip i {
+  margin-left: 5px;
+}
+
+.tooltip-text {
+  visibility: hidden;
+  width: 400px;
+  background-color: #333;
+  color: #fff;
+  text-align: center;
+  border-radius: 6px;
+  padding: 5px;
+  position: absolute;
+  z-index: 1;
+  top: 100%;
+  left: 50%;
+  transform: translateX(-50%);
+  opacity: 0;
+  transition: opacity 0.3s;
+}
+
+.tooltip:hover .tooltip-text {
+  visibility: visible;
+  opacity: 1;
+}
+
+.error-message {
+  color: red;
+  display: flex;
+  align-items: center;
+}
+
+.error-message i {
+  margin-right: 5px;
 }
 </style>
