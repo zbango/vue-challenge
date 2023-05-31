4911b16dd7c4cc5426502d9945dee4eb4172c003
Steven Tabango <steven.tabango@gmail.com>
2023-05-30 21:25:46 -0500

    feat: Create HotelGrid component

---
+++ src/components/HotelGrid.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="hotel-grid">
+    <HotelCard v-for="hotel in hotelStore.hotels" :key="hotel.id" :hotel="hotel" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useHotelStore } from '../stores/hotels'
+import HotelCard from './HotelCard.vue'
+
+const hotelStore = useHotelStore()
+</script>
+
+<style scoped>
+.hotel-grid {
+  display: flex;
+  justify-content: center;
+  gap: 20px;
+  flex-wrap: wrap;
+}
+
+@media (max-width: 768px) {
+  .hotel-grid {
+    justify-content: flex-start;
+  }
+}
+</style>
